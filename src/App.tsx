60ee5910b508a07fad84cdb567af526749e43358
AtharvaNagarsekar <atharvanagarsekar17@gmail.com>
2026-03-09 22:40:23 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
